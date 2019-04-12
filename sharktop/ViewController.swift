4a6099e67c8ca47b049217497bfebe3c1bd94250
aluno <aluno@LABMAC14.local>
2019-04-11 21:35:29 -0300

    first

--- sharktop/ViewController.swift
+++ sharktop/ViewController.swift
@@ -11,6 +11,9 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
+    var nodes = [SCNNode]()
+    var cuboNode = SCNNode()
+
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -23,11 +26,154 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        sceneView.debugOptions = [SCNDebugOptions.showFeaturePoints]
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        createCube()
+        
+//        importHuman(qntdHuman: 50)
+    }
+    @IBAction func chooseLvl1(_ sender: UIButton) {
+        for node in nodes{
+            node.removeFromParentNode()
+        }
+        importHuman(qntdHuman: 15)
+        
+    }
+    
+    @IBAction func chooseLvl2(_ sender: UIButton) {
+        for node in nodes{
+            node.removeFromParentNode()
+        }
+        importHuman(qntdHuman: 20)
+    }
+    
+    @IBAction func chooseLvl3(_ sender: UIButton) {
+        for node in nodes{
+            node.removeFromParentNode()
+        }
+        importHuman(qntdHuman: 30)
+    }
+    
+    private func importHuman(qntdHuman: Int) {
+
+    
+        var inicio: Int!
+        
+        inicio = 1
+        
+        while inicio <= qntdHuman{
+            let scene = SCNScene(named: "art.scnassets/lumberJack.DAE")!
+            let numberZ = Int.random(in: -10 ..< 10)
+            let numberY = Int.random(in: -20 ..< 20)
+            let numberX = Int.random(in: -3 ..< 3)
+            print(inicio)
+            if let human = scene.rootNode.childNode(withName: "lumberJack", recursively: true) {
+                human.position = SCNVector3(numberY, numberX, numberZ)
+                sceneView.scene.rootNode.addChildNode(human)
+                nodes.append(human)
+                let animation = SCNAction.rotateBy(x: 0, y: CGFloat(Float.pi * 10), z: 0, duration: 2)
+                let action = SCNAction.repeatForever(animation)
+                
+                human.runAction(action)
+                
+                let vector = SCNVector3(
+                                        cuboNode.position.x,
+                                        cuboNode.position.y,
+                                        cuboNode.position.z
+//                    cuboNode.worldTransform.cumns.3.x
+                                    )
+                
+                human.runAction(SCNAction.move(to: vector, duration: 12))
+                
+                
+            }
+//            print(nodes)
+            inicio = inicio + 1
+        }
+        
+    }
+    
+    private func animation(velocidade: Int){
+        
+    }
+    
+    
+//    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+//        if let anchor = anchor as? ARPlaneAnchor {
+////            createPlane(node: node, anchor: anchor)
+//
+//
+//        }
+//    }
+
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            
+            
+            let newResults = sceneView.hitTest(touchLocation, options: nil)
+            newResults.first?.node.removeFromParentNode()
+            
+            
+            
+//            if let hitResult = newResults.first {
+//                //                print(hitResult)
+//
+//
+////                let vector = SCNVector3(
+////                    hitResult.node.worldTransform.columns.3.x,
+////                    hitResult.worldTransform.columns.3.y,
+////                    hitResult.worldTransform.columns.3.z
+////                )
+////
+//                createCube(vector: newResults.first?.node)
+//            }
+        }
+    }
+    
+    private func createCube() {
+        // Creates a rectangle
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        
+        // Creates a material for the rectangle. The material is what is made of.
+        let material = SCNMaterial()
+        
+        // Sets the material to be red. This will cause the cube to be all read
+        material.diffuse.contents = UIColor.red
+        
+        // Then apply the materials to the cube
+        cube.materials = [material]
+        
+        // Creates a node. This is a 3D position (x, y, z). Remember that when the Z is increased, it is coming towards.
+        
+        let node = SCNNode()
+        node.position = SCNVector3(0, 0, -0.5)
+        
+        // Apply the node geometry it will be linked with
+        node.geometry = cube
+        
+        self.cuboNode = node
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
+    }
+
+    
+    private func createPlane(node: SCNNode, anchor: ARPlaneAnchor) {
+        let plane = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+        
+//        let ball = SCNSphere
+        
+        let planeNode = SCNNode()
+        planeNode.position = SCNVector3(anchor.center.x, 0, anchor.center.z)
+        
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+        
+        planeNode.geometry = plane
+        
+        node.addChildNode(planeNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,9 +181,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
